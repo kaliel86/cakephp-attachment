4552e78a0f6929c20da16e8ed3f99c0731146480
maxime CHENE <maxime@kaliel.fr>
2020-03-01 22:37:32 +0100

    init associations

--- src/Model/Behavior/AttachmentBehavior.php
+++ src/Model/Behavior/AttachmentBehavior.php
@@ -5,6 +5,7 @@ namespace Attachment\Model\Behavior;
 
 use Cake\ORM\Behavior;
 use Cake\ORM\Table;
+use Cake\ORM\TableRegistry;
 
 /**
  * Attachment behavior
@@ -19,6 +20,31 @@ class AttachmentBehavior extends Behavior
     protected $_defaultConfig = [
         'table' => 'attachments',
         'path' => WWW_ROOT . 'attachments',
-        'fields' => []
+        'associations' => []
     ];
+    /**
+     * @var \Cake\ORM\Table
+     */
+    private $attachmentsTable;
+
+    /**
+     * @param array $config
+     */
+    public function initialize(array $config): void
+    {
+        $this->attachmentsTable = TableRegistry::getTableLocator()->get($this->getConfig('table'));
+        $this->setupAssociations();
+    }
+
+    protected function setupAssociations()
+    {
+        $currentTable = $this->getTable();
+
+        $currentTable->hasMany('Attachments_images')
+            ->setJoinType('LEFT')
+            ->setForeignKey('foreign_key')
+            ->setProperty('images');
+
+        debug($currentTable->associations());
+    }
 }
